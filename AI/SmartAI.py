bec745952b7fef8885f0a045ecad13710a260938
Zachary Lohner <zkatana@gmail.com>
2016-11-03 13:59:24 -0600

    Fix infinite recursion bug, add full-tree exploration for the endgame, add alphabeta pruning

--- AI/SmartAI.py
+++ AI/SmartAI.py
@@ -2,12 +2,12 @@
 
 from AI import AI
 
-TOTAL_SLOTS = 64
+TOTAL_MOVES = 64
 
 # class to represent a state and its relevant information
 # TODO: implement memoization of previously computed nodes
 class Node(object):
-	def __init__(self, state, round, row, col, depth=2, max=True, alpha=-float('inf'), beta=float('inf')):
+	def __init__(self, state, round, row, col, depth=5, max=True, alpha=-float('inf'), beta=float('inf')):
 		self.state = state
 		self.round = round
 		self.value = -1
@@ -22,14 +22,19 @@ class SmartAI(AI):
 	def __init__(self, me):
 		AI.__init__(self, me)
 
-	# count the final score of a finished game; used when few moves remain to explore the whole tree
+	# count my current score
 	def countScore(self, state):
 		score = 0
 		for row in state:
 			for slot in row:
 				if slot == self.me:
 					score += 1
-		if score > TOTAL_SLOTS - score:
+		return score
+
+	# count the final score of a finished game; used when few moves remain to explore the whole tree
+	def checkWinner(self, state):
+		score = self.countScore(state)
+		if score > TOTAL_MOVES - score:
 			return float('inf')
 		else:
 			return -float('inf')
@@ -48,6 +53,12 @@ class SmartAI(AI):
 	# evaluate how good a given board state is
 	# calculated on a zero-sum basis, positive is me, negative is opponent
 	def heuristic(self, node):
+
+		pivotTurn = 10
+
+		if TOTAL_MOVES - node.round < pivotTurn:
+			return self.countScore(node.state)
+
 		score = 0
 
 		"""
@@ -90,8 +101,8 @@ class SmartAI(AI):
 	# explore a node using minimax adversarial search with limited depth and a heuristic function
 	# TODO: implement alpha-beta pruning
 	def minimax(self, node):
-		if TOTAL_SLOTS - node.round == 0:
-			node.value = self.countScore(node.state)
+		if TOTAL_MOVES - node.round == 0:
+			node.value = self.checkWinner(node.state)
 
 		if node.depth == 0:
 			node.value = self.heuristic(node)
@@ -108,17 +119,30 @@ class SmartAI(AI):
 					move[0],
 					move[1],
 					depth=(node.depth - 1),
-					max=False
+					max=False,
+					alpha=node.alpha,
+					beta=node.beta
 				)
 				child.value = self.minimax(child).value
 				children.append(child)
+
+				node.alpha = max(node.alpha, child.value)
+				if node.beta <= node.alpha:
+					break
+
 			if len(validMoves) > 0:
 				best = max(children, key=lambda node: node.value)
 				node.value = best.value
 				return best
 			else:
-				node.max = False
-				return self.minimax(node)
+				oppValidMoves = self.getValidMoves(node.state, node.round, self.opp)
+				if len(oppValidMoves) > 0:
+					node.max = False
+					return self.minimax(node)
+				else:
+					node.value = self.countScore(node.state)
+					return node
+
 		else:
 			validMoves = self.getValidMoves(node.state, node.round, self.opp)
 			for move in validMoves:
@@ -129,17 +153,29 @@ class SmartAI(AI):
 					move[0],
 					move[1],
 					depth=(node.depth - 1),
-					max=False
+					max=True,
+					alpha=node.alpha,
+					beta=node.beta
 				)
 				child.value = self.minimax(child).value
 				children.append(child)
+
+				node.beta = min(node.beta, child.value)
+				if node.beta <= node.alpha:
+					break
+
 			if len(validMoves) > 0:
 				best = min(children, key=lambda node: node.value)
 				node.value = best.value
 				return best
 			else:
-				node.max = True
-				return self.minimax(node)
+				oppValidMoves = self.getValidMoves(node.state, node.round, self.me)
+				if len(oppValidMoves) > 0:
+					node.max = True
+					return self.minimax(node)
+				else:
+					node.value = self.countScore(node.state)
+					return node
 
 	# get move (uses minimax)
 	def move(self, **kwargs):
